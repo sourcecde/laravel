c8083fde1b62cdb06ab4c06a1793abb6c28f47c9
Unknown <dbrj.prof@gmail.com>
2018-08-03 02:58:24 +0530

    user model

--- app/user.php
+++ app/user.php
@@ -8,6 +8,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class user extends Authenticatable
 {
     public function passport() {
-        // body...
+        return $this->hasOne(Passport::class);
+    }
+
+    public function mobile() {
+        return $this->hasMany(Mobile::class);
     }
 }
